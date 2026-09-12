dc1748e2148a2256b2c75717112798d2bc47e7e4
luisaguiar659-lang <luis.aguiar659@gmail.com>
2026-09-12 16:18:01 -0300

    Create message model for chat system

---
+++ mobile/lib/models/message.dart
@@ -0,0 +1,11 @@
+class Message {
+  final String text;
+  final bool isAgent;
+  final DateTime time;
+
+  Message({
+    required this.text,
+    required this.isAgent,
+    required this.time,
+  });
+}
