b39a631b2189f0417fe32e6a11214d517574533e
luisaguiar659-lang <luis.aguiar659@gmail.com>
2026-09-12 16:34:01 -0300

    Add user profile model

---
+++ mobile/lib/models/user_profile.dart
@@ -0,0 +1,11 @@
+class UserProfile {
+  final String name;
+  final String email;
+  final bool online;
+
+  UserProfile({
+    required this.name,
+    required this.email,
+    required this.online,
+  });
+}
