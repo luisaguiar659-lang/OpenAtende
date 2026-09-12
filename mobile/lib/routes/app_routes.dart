07d145c7b4d28c3439547a4ac08197df1028605d
luisaguiar659-lang <luis.aguiar659@gmail.com>
2026-09-12 16:27:30 -0300

    Add login route for MVP app flow

--- mobile/lib/routes/app_routes.dart
+++ mobile/lib/routes/app_routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import '../screens/conversations_screen.dart';
+import '../screens/login_screen.dart';
 
 class AppRoutes {
   static Map<String, WidgetBuilder> routes = {
+    '/login': (context) => const LoginScreen(),
     '/conversations': (context) => const ConversationsScreen(),
   };
 }
